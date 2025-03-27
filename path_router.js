74c660dd296c3428ace1bdf75e068fb88a0435bd
= <max.gorsty@gmail.com>
2025-03-27 17:47:39 +1300

    Deleted redundant files i.e gradle build files

--- path_router.js
+++ path_router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-// const pool = require('./db');
+const pool = require('./db');
 const path = require('path');
 router = express.Router();
 
@@ -9,6 +9,18 @@ router.use(fileUpload());
 router.use(express.static('public'));
 
 router.get('/', async (req, res) => {
+    const db = pool.promise();
+    
+    query = "select * from Flat;";
+
+    try{
+        const [rows, fields] = await db.query(query);
+        data = rows
+        console.log(data);
+    }catch (err){
+        console.error("query didn't work", err);
+    }
+
     res.render('groceries'); //change later when homepage is created
 });
 
